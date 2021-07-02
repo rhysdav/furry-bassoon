902802219980f92c9acc5f076fa5e2bc6870a52e
Hannah Davies <hannahdavies@rhys-mac.local>
2021-07-02 14:26:57 -0600

    Init Spring Project

---
+++ src/main/java/com/furrybassoon/furrybassoon/FurryBassoonApplication.java
@@ -0,0 +1,13 @@
+package com.furrybassoon.furrybassoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurryBassoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FurryBassoonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/furrybassoon/furrybassoon/FurryBassoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furrybassoon.furrybassoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurryBassoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
